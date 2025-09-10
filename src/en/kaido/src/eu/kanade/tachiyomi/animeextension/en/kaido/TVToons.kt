95f5226bc2350dfa2d15acb04c410700e59e8408
deepmalarao0-ui <deepmalarao0@gmail.com>
2025-09-11 02:17:32 +0530

    Update TVToons.kt

--- src/en/kaido/src/eu/kanade/tachiyomi/animeextension/en/kaido/TVToons.kt
+++ src/en/kaido/src/eu/kanade/tachiyomi/animeextension/en/kaido/TVToons.kt
@@ -3,14 +3,19 @@ package eu.kanade.tachiyomi.animeextension.en.tvtoons
 import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
 import eu.kanade.tachiyomi.multisrc.zorotheme.ZoroTheme
 import eu.kanade.tachiyomi.lib.rapidcloudextractor.RapidCloudExtractor
-import kotlin.getValue
 
 class TVToons : ZoroTheme(
-    "en",
-    "TVToons",
-    "https://tvtoons.co",
-    hosterNames = listOf(
+    "en",                     // Language
+    "TVToons",                // Source name
+    "https://tvtoons.co",     // Base URL
+    hosterNames = listOf(     // Supported video hosts
         "Vidstreaming",
         "Vidcloud",
     ),
-)
+) {
+    // Enable RapidCloud extractor for video playback
+    override val useRapidCloudExtractor = true
+
+    // Filters (empty if TVToons has no advanced filters)
+    override fun getFilterList(): AnimeFilterList = AnimeFilterList()
+}
