21118eaea72fab05b62c116ba57b2f01f0fad46c
deepmalarao0-ui <deepmalarao0@gmail.com>
2025-09-11 03:03:21 +0530

    Update TVToons.kt

--- src/en/kaido/src/eu/kanade/tachiyomi/animeextension/en/kaido/TVToons.kt
+++ src/en/kaido/src/eu/kanade/tachiyomi/animeextension/en/kaido/TVToons.kt
@@ -1,17 +1,17 @@
 package eu.kanade.tachiyomi.animeextension.en.tvtoons
 
 import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
-import eu.kanade.tachiyomi.multisrc.zorotheme.ZoroTheme
 import eu.kanade.tachiyomi.lib.rapidcloudextractor.RapidCloudExtractor
+import eu.kanade.tachiyomi.multisrc.zorotheme.ZoroTheme
 
 class TVToons : ZoroTheme(
-    "en",                     // Language
-    "TVToons",                // Source name
-    "https://tvtoons.co",     // Base URL
-    hosterNames = listOf(     // Supported video hosts
+    "en",                   // Language
+    "TVToons",              // Source name
+    "https://tvtoons.co",   // Base URL
+    hosterNames = listOf(   // Supported video hosts
         "Vidstreaming",
-        "Vidcloud",
-    ),
+        "Vidcloud"
+    )
 ) {
     // Enable RapidCloud extractor for video playback
     override val useRapidCloudExtractor = true
